b99766da8850fff3abe9e16c3826b7be211b5b96
Bernhard M. Wiedemann <bernhard+smartdiffgit lsmod.de>
2011-01-10 19:36:10 +0100

    add bzip2 compression support

--- handler.d/compressed.pm
+++ handler.d/compressed.pm
@@ -1,10 +1,15 @@
+#!/usr/bin/perl -w
 # uncompress gzip
 package smartdiff::compressed;
 use base 'basehandler';
+use strict;
 
+my $compressor;
 sub can_handle($)
 { my($class,$type)=@_;
-	$type=~m/^[a-zA-Z0-9]+ compressed/;
+	my $match=$type=~m/^([a-zA-Z0-9]+) compressed/;
+  $compressor=$1;
+  return $match;
 }
 
 sub diffdata
@@ -20,7 +25,7 @@ sub prepare
 #	open(my $fd, "-|", "gzip", "-cd", $fn) or die $!;
 	open my $oldout,     ">&", \*STDOUT or die "Can't dup STDOUT: $!";
 	open(STDOUT, ">&", $outfd);
-	system("gzip", "-cd", $fn);
+	system($compressor, "-cd", $fn);
 	open STDOUT, ">&", $oldout or die "Can't dup \$oldout: $!";
 	close($outfd);
 }
